e5f5c552de2b9337d7ad51175b97710ebc767473
Kingsley Albert <Kingsley.Albert@safetyio.com>
2024-04-22 07:16:53 +0200

    committed_initial_files

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,76 @@
+import 'package:ex_2/utils/authentication.dart';
+import 'package:ex_2/widgets/google_sign_in_button.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            children: [
+              const Row(),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Image.asset(
+                        'assets/images/firebase_logo.png',
+                        height: 160,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'FlutterFire',
+                      style: TextStyle(
+                        color: Colors.yellow,
+                        fontSize: 40,
+                      ),
+                    ),
+                    const Text(
+                      'Authentication',
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FutureBuilder(
+                future: Authentication.initializeFirebase(context: context),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error initializing Firebase');
+                  } else if (snapshot.connectionState == ConnectionState.done) {
+                    return GoogleSignInButton();
+                  }
+                  return CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      Colors.orange,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
